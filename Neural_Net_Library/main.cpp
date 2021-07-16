b6983320e48a3cc56e3d0864a7d538966775c883
Thiviyan Saravanamuthu <thivi30082006@outlook.com>
2021-07-16 12:39:11 +0200

    Removal of Data Managment(Useless feature)

--- Neural_Net_Library/main.cpp
+++ Neural_Net_Library/main.cpp
@@ -13,7 +13,8 @@ int main()
     
     std::vector<int> topology;
     topology.push_back(2);
-    topology.push_back(5);
+    topology.push_back(2);
+    topology.push_back(3);
     topology.push_back(1);
     
     NN::NN NeuralNet = NN::NN(topology,
